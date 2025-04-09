ed175f6be226bd37a4422ad3a056db4b630bf6dd
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-04-09 23:51:13 +0700

    Menambahkan AboutScreen

---
+++ app/src/main/java/com/ilhamalgojali0081/assesment_1/ui/theme/screen/AboutScreen.kt
@@ -0,0 +1,62 @@
+package com.ilhamalgojali0081.assesment_1.ui.theme.screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ilhamalgojali0081.assesment_1.R
+import com.ilhamalgojali0081.assesment_1.ui.theme.Assesment_1Theme
+import com.ilhamalgojali0081.assesment_1.ui.theme.poppins
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AboutScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.about_screen),
+                        fontFamily = poppins,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        }
+    ) { innerPadding ->
+        Text(
+            text = stringResource(R.string.app_description),
+            modifier = Modifier.padding(innerPadding).padding(16.dp)
+        )
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Preview(showBackground = true)
+@Composable
+fun AboutScreenPreview(){
+    Assesment_1Theme {
+        AboutScreen()
+    }
+}
+
