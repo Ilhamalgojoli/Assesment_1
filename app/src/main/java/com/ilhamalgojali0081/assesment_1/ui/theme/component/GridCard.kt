9fd66ffa69ae99304e45dad14902dfd00e7eb685
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-05-06 00:41:09 +0700

    Membuat tampilan GridCard

---
+++ app/src/main/java/com/ilhamalgojali0081/assesment_1/ui/theme/component/GridCard.kt
@@ -0,0 +1,82 @@
+package com.ilhamalgojali0081.assesment_1.ui.theme.component
+
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ilhamalgojali0081.assesment_1.model.Category
+import com.ilhamalgojali0081.assesment_1.model.Product
+import com.ilhamalgojali0081.assesment_1.relatiion.CategoryWithProduct
+import com.ilhamalgojali0081.assesment_1.ui.theme.Assesment_1Theme
+import com.ilhamalgojali0081.assesment_1.ui.theme.poppins
+
+@Composable
+fun GridCard(categoryWithProduct: CategoryWithProduct, onClick: () -> Unit){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable{ onClick() },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        border = BorderStroke(1.dp, DividerDefaults.color)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = categoryWithProduct.product.name,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold,
+                fontFamily = poppins,
+                maxLines = 1
+            )
+            Text(
+                text = "Category : ${ categoryWithProduct.category.name }",
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Normal,
+                fontFamily = poppins,
+                maxLines = 1
+            )
+            Text(
+                text = "Stock : ${ categoryWithProduct.product.quantity }",
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Normal,
+                fontFamily = poppins,
+                maxLines = 1
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun GridPreview(){
+    Assesment_1Theme {
+        val dummyCategory = CategoryWithProduct(
+            category = Category(id = 1, name = "Electronics"),
+            product = Product(id = 1, name = "Smartphone",4," " , 5.0,categoryId = 1)
+        )
+        GridCard (
+            categoryWithProduct = dummyCategory,
+            onClick = { }
+        )
+    }
+}
