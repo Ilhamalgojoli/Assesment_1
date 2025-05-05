789ee60fd038fa554ac3cf80b7ceecf91cb7ecff
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-05-06 00:35:45 +0700

    Memperbarui ListCard

--- app/src/main/java/com/ilhamalgojali0081/assesment_1/ui/theme/component/ListCard.kt
+++ app/src/main/java/com/ilhamalgojali0081/assesment_1/ui/theme/component/ListCard.kt
@@ -1,154 +1,73 @@
 package com.ilhamalgojali0081.assesment_1.ui.theme.component
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.ilhamalgojali0081.assesment_1.R
+import com.ilhamalgojali0081.assesment_1.model.Category
 import com.ilhamalgojali0081.assesment_1.model.Product
+import com.ilhamalgojali0081.assesment_1.relatiion.CategoryWithProduct
 import com.ilhamalgojali0081.assesment_1.ui.theme.poppins
 
 @Composable
-fun ListCard(product: Product,onEdit: (Product) -> Unit,onDelete: (Product) -> Unit ) {
-
-    val context = LocalContext.current
-
-    Card(
+fun ListCard(categoryWithProduct: CategoryWithProduct,onClick: () -> Unit) {
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(12.dp),
-        border = BorderStroke(1.dp, Color.Black),
-        colors = CardDefaults.cardColors(containerColor = Color.White)
+            .clickable{ onClick() }
+            .padding(8.dp)
+            .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(4.dp)),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         Column(
             modifier = Modifier
                 .padding(8.dp)
-                .fillMaxWidth()
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Image(
-                    painter = painterResource(id = product.icon),
-                    contentDescription = product.name,
-                    modifier = Modifier
-                        .padding(start = 16.dp)
-                        .size(100.dp)
+            Text(
+                text = categoryWithProduct.product.name,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold,
+                fontFamily = poppins,
+                maxLines = 1
+            )
+            Text(
+                text = "Category : ${ categoryWithProduct.category.name }",
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Normal,
+                fontFamily = poppins,
+                maxLines = 1
                 )
-                Column(
-                    modifier = Modifier
-                        .padding(vertical = 8.dp, horizontal = 15.dp)
-                        .weight(1f)
-                ) {
-                    Text(
-                        text = product.name,
-                        fontFamily = poppins,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 20.sp,
-                        color = Color.Black
-                    )
-                    Text(
-                        text = stringResource(R.string.quantity_in_list) + product.quantity,
-                        fontFamily = poppins,
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 14.sp,
-                        color = Color.Black
-                    )
-                    Text(
-                        text = stringResource(R.string.tanggal_masuk) + product.stokInDate,
-                        fontFamily = poppins,
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 14.sp,
-                        color = Color.Black
-                    )
-                }
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp, horizontal = 12.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Button(
-                    onClick = {
-                        shareData(
-                            context = context,
-                            message = context.getString(
-                                R.string.share_product,
-                                product.name,
-                                product.quantity,
-                                product.stokInDate,
-                            )
-                        )
-                    },
-                    contentPadding = PaddingValues(vertical = 8.dp, horizontal = 12.dp)
-                ) {
-                    Text(text = stringResource(R.string.share))
-                }
-                Button(
-                    onClick = { onEdit(product) },
-                    contentPadding = PaddingValues(vertical = 8.dp, horizontal = 12.dp)
-                ) {
-                    Text(text = stringResource(R.string.edit))
-                }
-                Button(
-                    onClick = { onDelete(product) },
-                    contentPadding = PaddingValues(vertical = 8.dp, horizontal = 12.dp)
-                ) {
-                    Text(text = stringResource(R.string.delete))
-                }
-            }
+            Text(
+                text = "Stock : ${ categoryWithProduct.product.quantity }",
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Normal,
+                fontFamily = poppins,
+                maxLines = 1
+            )
         }
     }
-
 }
 
-private fun shareData(context: Context, message: String){
-    val sharedIntent = Intent(Intent.ACTION_SEND).apply {
-        type = "text/plain"
-        putExtra(Intent.EXTRA_TEXT, message)
-    }
-    if(sharedIntent.resolveActivity(context.packageManager) != null){
-        context.startActivity(sharedIntent)
-    }
-}
-
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun ListCardPreview(){
-    val dummyProduct = Product(
-        id = "blabla",
-        name = "Contoh Produk",
-        icon = R.drawable.laptop,
-        quantity = "10",
-        stokInDate = "2025-04-07"
+    val dummyCategory = CategoryWithProduct(
+        category = Category(id = 1, name = "Electronics"),
+        product = Product(id = 1, name = "Smartphone",4," " , 5.0,categoryId = 1)
     )
-
     ListCard(
-        product = dummyProduct,
-        onEdit = {},
-        onDelete = {}
+        categoryWithProduct = dummyCategory,
+        onClick = { }
     )
 }
